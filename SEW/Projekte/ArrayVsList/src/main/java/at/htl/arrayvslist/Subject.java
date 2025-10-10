5dcab7ebd96346a06b9497c1286e2495233500a8
Adrian Fritz <adrian.fritz@it.htl-hl.ac.at>
2025-10-10 10:31:47 +0200

    implemented Subject

--- SEW/Projekte/ArrayVsList/src/main/java/at/htl/arrayvslist/Subject.java
+++ SEW/Projekte/ArrayVsList/src/main/java/at/htl/arrayvslist/Subject.java
@@ -1,4 +1,44 @@
 package at.htl.arrayvslist;
 
 public class Subject {
+
+    // Fields *****************************************************************
+
+    private String name;
+    private String teacher;
+    private int weeklyHours;
+
+    // Instance creation ******************************************************
+
+    public Subject(String name, String teacher, int weeklyHours) {
+        this.name = name;
+        this.teacher = teacher;
+        this.weeklyHours = weeklyHours;
+    }
+
+    // Accessors **************************************************************
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public int getWeeklyHours() {
+        return weeklyHours;
+    }
+
+    public void setWeeklyHours(int weeklyHours) {
+        this.weeklyHours = weeklyHours;
+    }
 }
