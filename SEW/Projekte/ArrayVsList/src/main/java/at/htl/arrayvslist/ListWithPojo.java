f9e0e93d9046e84e58476d64e8e837b5b57f25dc
Adrian Fritz <adrian.fritz@it.htl-hl.ac.at>
2025-10-10 10:35:46 +0200

    implemented ListWithPojo

--- SEW/Projekte/ArrayVsList/src/main/java/at/htl/arrayvslist/ListWithPojo.java
+++ SEW/Projekte/ArrayVsList/src/main/java/at/htl/arrayvslist/ListWithPojo.java
@@ -1,4 +1,13 @@
 package at.htl.arrayvslist;
 
+import java.util.ArrayList;
+
 public class ListWithPojo {
+
+    ArrayList<Subject> subjectList;
+
+    public ListWithPojo() {
+
+        subjectList = new ArrayList<Subject>();
+    }
 }
