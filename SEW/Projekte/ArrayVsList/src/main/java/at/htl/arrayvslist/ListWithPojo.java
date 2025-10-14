6134df46d2752af11ec1ea70ecda731513821ff0
Adrian Fritz <adrian.fritz@it.htl-hl.ac.at>
2025-10-14 10:58:19 +0200

    added whole timetable

--- SEW/Projekte/ArrayVsList/src/main/java/at/htl/arrayvslist/ListWithPojo.java
+++ SEW/Projekte/ArrayVsList/src/main/java/at/htl/arrayvslist/ListWithPojo.java
@@ -20,8 +20,42 @@ public class ListWithPojo {
     }
 
     private void fillList(){
-        subjectList.add(new Subject("SEW1", "GEI", 1));
+        // Monday
+        subjectList.add(new Subject("INSY", "HOH", 2));
+        subjectList.add(new Subject("AM", "PRM", 2));
+        subjectList.add(new Subject("BESP", "WEF", 2));
+        subjectList.add(new Subject("GGP", "PAUL", 1));
+        subjectList.add(new Subject("NWT1", "FIR, LIDL", 1));
+        subjectList.add(new Subject("ITSI_1", "FIR, LIDL", 1));
+
+        // Tuesday
+        subjectList.add(new Subject("NW", "SWA", 1));
+        subjectList.add(new Subject("D", "NEUB", 1));
         subjectList.add(new Subject("SEW", "GEI", 2));
+        subjectList.add(new Subject("D", "NEUB", 1));
+        subjectList.add(new Subject("GGP", "PAUL", 1));
+        subjectList.add(new Subject("MEDT", "FICI, GRU", 2));
+
+        // Wednesday
+        subjectList.add(new Subject("ITSI_1", "EBE", 1));
+        subjectList.add(new Subject("NWT1", "LIDL", 1));
+        subjectList.add(new Subject("E", "GOLL", 1));
+        subjectList.add(new Subject("SYT", "ZOG", 1));
+        subjectList.add(new Subject("RK", "SCHM", 2));
+
+        // Thursday
+        subjectList.add(new Subject("ITP", "RAH", 4));
+        subjectList.add(new Subject("INSY1", "HOH, STO", 1));
+
+        // Friday
+        subjectList.add(new Subject("NW", "SWA", 1));
+        subjectList.add(new Subject("AM", "PRM", 1));
+        subjectList.add(new Subject("SEW1", "GEI", 1));
+        subjectList.add(new Subject("E", "GOLL", 1));
+        subjectList.add(new Subject("NWT1", "LIDL", 1));
+        subjectList.add(new Subject("ITP1", "RAH", 1));
+        subjectList.add(new Subject("SYT_U", "FIMI, HOH", 2));
+        subjectList.add(new Subject("SYT", "HOH", 1));
     }
 
     public static void main(String[] args) {
