de166babe24f94ebf787cad565e1e2f4446ebf50
Adrian Fritz <adrian.fritz@it.htl-hl.ac.at>
2025-10-14 10:40:42 +0200

    implemented ListWithPojo

--- SEW/Projekte/ArrayVsList/src/main/java/at/htl/arrayvslist/ListWithPojo.java
+++ SEW/Projekte/ArrayVsList/src/main/java/at/htl/arrayvslist/ListWithPojo.java
@@ -9,5 +9,22 @@ public class ListWithPojo {
     public ListWithPojo() {
 
         subjectList = new ArrayList<Subject>();
+        fillList();
+
+        // Check weekly hours
+        int totalWeeklyHours = 0;
+        for (Subject subject : subjectList) {
+            totalWeeklyHours = totalWeeklyHours + subject.getWeeklyHours();
+        }
+        System.out.println("Total weekly hours: " + totalWeeklyHours);
+    }
+
+    private void fillList(){
+        subjectList.add(new Subject("SEW1", "GEI", 1));
+        subjectList.add(new Subject("SEW", "GEI", 2));
+    }
+
+    public static void main(String[] args) {
+        new ListWithPojo();
     }
 }
